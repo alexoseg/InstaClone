6013363227cf9adb3e0b8767b0a8d595fa6044ff
alexoseg <aoseg@stanford.edu>
2020-07-10 12:01:01 -0700

    Cleaned up CreateAccountViewController

--- InstaClone/View Controllers/CreateAccountViewController.m
+++ InstaClone/View Controllers/CreateAccountViewController.m
@@ -18,26 +18,29 @@
 
 @implementation CreateAccountViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-
 - (void) displayAlertWithMessage:(NSString *)alertMessage andTitle:(NSString *)titleMessage{
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:titleMessage message:alertMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:titleMessage
+                                                                   message:alertMessage
+                                                            preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
-      style:UIAlertActionStyleDefault
-    handler:^(UIAlertAction * _Nonnull action) {
-    }];
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:nil];
     [alert addAction:okAction];
-    [self presentViewController:alert animated:YES completion:^{
-    }];
+    [self presentViewController:alert
+                       animated:YES
+                     completion:nil];
 }
 
--(void)performSignUp{
-    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
-    
+- (IBAction)signUpUser:(id)sender {
+    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder)
+                                               to:nil
+                                             from:nil
+                                         forEvent:nil];
     typeof(self) __weak weakSelf = self;
-    [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    [MBProgressHUD showHUDAddedTo:self.view
+                         animated:YES];
+    
     [ParsePoster createAccountWithUsername:self.usernameTextField.text
                                   password:self.passwordTextField.text
                             withCompletion:^(BOOL succeeded, NSError * _Nullable error) {
@@ -47,29 +50,17 @@
             return;
         }
         
-        [MBProgressHUD hideHUDForView:weakSelf.view animated:YES];
+        [MBProgressHUD hideHUDForView:weakSelf.view
+                             animated:YES];
         if (error != nil ) {
             NSLog(@"Error: %@", error.localizedDescription);
-            [strongSelf displayAlertWithMessage:error.localizedDescription andTitle:@"Sign up error"];
+            [strongSelf displayAlertWithMessage:error.localizedDescription
+                                       andTitle:@"Sign up error"];
         } else {
             NSLog(@"User Registed Successfully");
             [strongSelf performSegueWithIdentifier:@"signupCompleteSegue" sender:nil];
         }
     }];
 }
-
-- (IBAction)signUpUser:(id)sender {
-    [self performSignUp];
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
